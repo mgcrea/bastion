6ad6f6a8d44aa2fb25542639b88f52c844a2d74b
Olivier Louvignes <skater_talc_0f@icloud.com>
2026-08-29 16:33:11 +0200

    chore: initial Xcode macOS app template

---
+++ apps/apple/Bastion/BastionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BastionApp.swift
+//  Bastion
+//
+//  Created by Olivier Louvignes on 29/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct BastionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps/apple/Bastion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bastion
+//
+//  Created by Olivier Louvignes on 29/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
